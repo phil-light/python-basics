74d6b94ae265919b053f51a933ad0dc55151b450
phil light <plight@gmail.com>
2025-06-24 18:01:43 -0400

    mostly refactoring, moving functionality into util file
    I'm also working on creating a cell for students to try to navigate the
    maze with their turtle after drawing it.

--- maze_maker.py
+++ maze_maker.py
@@ -2,12 +2,153 @@ import random
 import ipyturtle3 as turtle
 from ipyturtle3 import hold_canvas
 
+
+colors = ['silver'
+,'gray'
+,'maroon'
+,'red'
+,'purple'
+,'fuchsia'
+,'green'
+,'lime'
+,'olive'
+,'yellow'
+,'navy'
+,'blue'
+,'teal'
+,'aqua'
+,'orange'
+,'aliceblue'
+,'antiquewhite'
+,'aquamarine'
+,'azure'
+,'beige'
+,'bisque'
+,'blanchedalmond'
+,'blueviolet'
+,'brown'
+,'burlywood'
+,'cadetblue'
+,'chartreuse'
+,'chocolate'
+,'coral'
+,'cornflowerblue'
+,'cornsilk'
+,'crimson'
+,'darkblue'
+,'darkcyan'
+,'darkgoldenrod'
+,'darkgray'
+,'darkgreen'
+,'darkkhaki'
+,'darkmagenta'
+,'darkolivegreen'
+,'darkorange'
+,'darkorchid'
+,'darkred'
+,'darksalmon'
+,'darkseagreen'
+,'darkslateblue'
+,'darkslategray'
+,'darkturquoise'
+,'darkviolet'
+,'deeppink'
+,'deepskyblue'
+,'dimgray'
+,'dodgerblue'
+,'firebrick'
+,'floralwhite'
+,'forestgreen'
+,'gainsboro'
+,'ghostwhite'
+,'gold'
+,'goldenrod'
+,'greenyellow'
+,'honeydew'
+,'hotpink'
+,'indianred'
+,'indigo'
+,'ivory'
+,'khaki'
+,'lavender'
+,'lavenderblush'
+,'lawngreen'
+,'lemonchiffon'
+,'lightblue'
+,'lightcoral'
+,'lightcyan'
+,'lightgoldenrodyellow'
+,'lightgray'
+,'lightgreen'
+,'lightpink'
+,'lightsalmon'
+,'lightseagreen'
+,'lightskyblue'
+,'lightslategray'
+,'lightsteelblue'
+,'lightyellow'
+,'limegreen'
+,'linen'
+,'mediumaquamarine'
+,'mediumblue'
+,'mediumorchid'
+,'mediumpurple'
+,'mediumseagreen'
+,'mediumslateblue'
+,'mediumspringgreen'
+,'mediumturquoise'
+,'mediumvioletred'
+,'midnightblue'
+,'mintcream'
+,'mistyrose'
+,'moccasin'
+,'navajowhite'
+,'oldlace'
+,'olivedrab'
+,'orangered'
+,'orchid'
+,'palegoldenrod'
+,'palegreen'
+,'paleturquoise'
+,'palevioletred'
+,'papayawhip'
+,'peachpuff'
+,'peru'
+,'pink'
+,'plum'
+,'powderblue'
+,'rosybrown'
+,'royalblue'
+,'saddlebrown'
+,'salmon'
+,'sandybrown'
+,'seagreen'
+,'seashell'
+,'sienna'
+,'skyblue'
+,'slateblue'
+,'slategray'
+,'snow'
+,'springgreen'
+,'steelblue'
+,'tan'
+,'thistle'
+,'tomato'
+,'turquoise'
+,'violet'
+,'wheat'
+,'whitesmoke'
+,'yellowgreen'
+,'rebeccapurple']
+
+
 # Directions: (dx, dy)
 DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-
-
 # Recursive backtracking maze generation
-def generate_maze(maze, maze_rows, maze_cols, x, y):
+def generate_maze(maze_rows, maze_cols, x, y):
+    # Maze grid: 0 = wall, 1 = path
+    maze = [[0 for _ in range(MAZE_COLS)] for _ in range(MAZE_ROWS)]
+
     maze[y][x] = 1
     dirs = DIRS[:]
     random.shuffle(dirs)
@@ -16,6 +157,8 @@ def generate_maze(maze, maze_rows, maze_cols, x, y):
         if 0 <= nx < maze_cols and 0 <= ny < maze_rows and maze[ny][nx] == 0:
             maze[y + dy][x + dx] = 1
             generate_maze(maze, maze_rows, maze_cols, nx, ny)
+    return maze
+
 
 def draw_maze(my_ts, my_canvas, canvas_width, canvas_height, cell_size, maze):
     my_ts.clear()
@@ -62,6 +205,18 @@ def draw_maze(my_ts, my_canvas, canvas_width, canvas_height, cell_size, maze):
                         else:
                             waller.penup()
                         waller.movedown(cell_size)
+
+
+def start_maze(runner):
+    runner.penup()
+    runner.home()
+    runner.left(90)
+    runner.forward(130)
+    runner.left(90)
+    runner.forward(230)
+    runner.left(180)
+    runner.pendown()
+    runner.color(random.choice(colors))
 
 
 if __name__ == '__main__':
